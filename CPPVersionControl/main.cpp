fe94353ac73243b0560fafe6e60a3bc19631dd45
Jack Star <nyanheinaung@gmail.com>
2022-05-14 15:52:14 +0900

    printing Hello World

--- CPPVersionControl/main.cpp
+++ CPPVersionControl/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 int main() 
 {
-	std::cout << "Main Function \n\n";
-	//turn the below line into comment for commit test
-//	std::cout << "Test for Commit \n\n";
+	std::cout << "Hello World! \n\n";
+	
 }
